c226401db023d2906819c28289a2224c6ee3f014
Sebastiaan <sebas.ram13@gmail.com>
2021-06-08 22:12:50 +0200

    Parameter change

--- main.sh
+++ main.sh
@@ -6,4 +6,4 @@
 #SBATCH --mail-user=sram@science.ru.nl
 #SBATCH --time=01:00:00
 #
-~/radboud-naco-project/env/bin/python3 ~/radboud-naco-project/Sklearn-neat/main.py --generations=2 --population_size=50 --population_limit=0.9 --train_size=0.001 --test_size=0.0002
+~/radboud-naco-project/env/bin/python3 ~/radboud-naco-project/Sklearn-neat/main.py --generations=50 --population_size=50 --population_limit=0.9 --train_size=0.01 --test_size=0.005
