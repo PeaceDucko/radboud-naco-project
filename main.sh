e403ee1f93b0cd25d84fb181a1fa1d57f55330ce
Sebastiaan <sebas.ram13@gmail.com>
2021-06-20 09:58:58 +0200

    Commit all

--- main.sh
+++ main.sh
@@ -6,4 +6,4 @@
 #SBATCH --mail-user=sram@science.ru.nl
 #SBATCH --time=144:00:00
 #
-~/radboud-naco-project/env/bin/python3 ~/radboud-naco-project/Sklearn-neat/main.py --generations=150 --population_size=50 --fitness_limit=0.9 --train_size=0.01 --test_size=0.005
+~/radboud-naco-project/env/bin/python3 ~/radboud-naco-project/Sklearn-neat/main.py --generations=50 --population_size=150 --fitness_limit=0.9 --train_size=0.01 --test_size=0.005
